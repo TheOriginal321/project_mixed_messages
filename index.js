e9d4b71684be74b857a3e2f772b039fcdc4acd9c
The_Original321 <jphutton@mail.com>
2023-03-04 14:37:45 -0600

    Connect local files to correct Github repo

---
+++ index.js
@@ -0,0 +1,19 @@
+//Object to hold Weather Conditions
+const weatherConditions = [
+    "Sunny",
+    "Rainy",
+    "Cloudy",
+    "Dusty",
+    "Snowy",
+    "Foggy",
+    "Froggy",
+    "Extra Dusty",
+    "Unbelievably Dusty",
+    "Raining Cats and Dogs",
+    "Tornado Warning!",
+    "Hailing (Golfball size)",
+    "Hailing (Baseball size)"
+]
+//Function to return 'predicted' weather for the morning, afternoon, and night
+
+//Test
